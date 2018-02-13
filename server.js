c5609a403a75a87b53981bb9e2a131a1787d1a1f
Andreya <avalabek@yahoo.com>
2018-02-13 09:36:12 -0700

    set up database

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require ("express");
+var bodyParser = require("body-parser");
+var methodOverride = require ("method-override");
+
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+//serve static contnet for the app from public directory
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use (bodyParser.json());
+
+//set handlebars
+var exphbs = require ("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//import routes
+var routes = require ("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("App listening at port: "+ PORT);
+});
